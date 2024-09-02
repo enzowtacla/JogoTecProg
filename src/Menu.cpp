c500bf4d305a3b08f978e6510196c32d7b7f1238
JaoBezerra <joaovitorbezerra2013@hotmail.com>
2024-09-01 22:28:55 -0300

    Ranking menu

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,9 +1,10 @@
 #include "../include/Menus/Menu.h"
+#include <fstream>
 
 using namespace Jogo;
 using namespace Menus;
 
-Menu* Menu::pMenu(nullptr);
+Menu *Menu::pMenu(nullptr);
 
 Menu::Menu() : Ente(IDs::ID::MenuPrincipal), botoesMenu(), criarFase(false), numFase(0), numJogadores(0)
 {
@@ -31,9 +32,10 @@ Menu::~Menu()
 {
 }
 
-Menu* Menu::getMenu()
+Menu *Menu::getMenu()
 {
-    if(!pMenu){
+    if (!pMenu)
+    {
         pMenu = new Menu();
     }
     return pMenu;
@@ -80,45 +82,60 @@ void Menu::setPos(int pos)
 
 void Menu::selecionado(int pos)
 {
-    switch(pos)
+    switch (pos)
     {
-        case(0):
-        {
-            criarFase = true;
-            numFase = 1;
-            pGG->limpaJanela();
-        }break;
-
-        case(1):
-        {
-            criarFase = true;
-            numFase = 2;
-            pGG->limpaJanela();
-        }break;
-
-        case(2):
-        {
-            numJogadores = 1;
-            std::cout << " " << numJogadores <<std::endl;
-        }break;
-
-        case(3):
-        {
-            numJogadores = 2;
-            std::cout << " " << numJogadores <<std::endl;
-        }break;
-
-        case(4):
-        {
-
-        }break;
-
-        case(5):
-        {
-            pGG->fecharJanela();
-        }break;
+    case (0):
+    {
+        criarFase = true;
+        numFase = 1;
+        pGG->limpaJanela();
     }
+    break;
 
+    case (1):
+    {
+        criarFase = true;
+        numFase = 2;
+        pGG->limpaJanela();
+    }
+    break;
+
+    case (2):
+    {
+        numJogadores = 1;
+        std::cout << " " << numJogadores << std::endl;
+    }
+    break;
+
+    case (3):
+    {
+        numJogadores = 2;
+        std::cout << " " << numJogadores << std::endl;
+    }
+    break;
+
+    case (4):
+    {
+        std::ifstream arquivo("Ranking.txt");
+
+        std::string line;
+        std::cout << "RANKING:" << std::endl;
+        while (std::getline(arquivo, line))
+        {
+            if (!line.empty())
+            {
+                std::cout << line << std::endl;
+            }
+        }
+    }
+    break;
+
+    case (5):
+    {
+        pGG->fecharJanela();
+    }
+    break;
+    }
 }
 
 const bool Menu::getCriarFase() const
