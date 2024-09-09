e8753ab99abef510bb53252ea24a3a4f156d8028
enzowtacla <119675568+enzowtacla@users.noreply.github.com>
2024-09-09 10:18:53 -0300

    Créditos

--- src/main.cpp
+++ src/main.cpp
@@ -8,3 +8,4 @@ int main()
 }
 
 //Fundo da fase FlorestaLava foi gerado pela IA LimeWire: https://limewire.com/
+//sprites: https://pixelfrog-assets.itch.io/pixel-adventure-1 e https://pixelfrog-assets.itch.io/pixel-adventure-2  
